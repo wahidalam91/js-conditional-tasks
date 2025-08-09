b7bbfbb48dd6372396b9bd035d3e2762c799e0c1
AbdelWahid-I <md.wahid104@gmail.com>
2025-08-10 05:35:59 +0600

    task-1 completed

---
+++ comparison.js
@@ -0,0 +1,43 @@
+// grater >
+// less <
+// grater than or equal >=
+// less than or equal <=
+// not equal !=
+// and &&
+// or ||
+
+
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+// console.log(5 == 10);
+
+
+// console.log(10 < 5);
+
+
+let result = 10 < "5";
+console.log(result);
+
+console.log(6 != 6);
+
+let score = 75;
+let grade;
+if (score >= 90) {
+ grade = "A";
+} else if (score >= 80) {
+ grade = "B";
+}
+console.log(grade);
+
+var num = 10;
+if (num > 15) {
+  console.log("Greater than 15");
+} else if (num > 5) {
+  console.log("Greater than 5 but less than or equal to 15");
+} else {
+  console.log("Less than or equal to 5");
+}
+
+
---
+++ task-1.js
@@ -0,0 +1,16 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+// let burgerPrice = 600;
+// let burgerPrice = 490;
+
+if(burgerPrice > 500){
+    console.log("Coke free");
+}else{
+    console.log("Price of Coke 30tk")
+}
